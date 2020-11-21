913de1447945603ff770a20217ae7078b90a34b4
demidraksin <greatraksin@icloud.com>
2020-11-21 16:09:10 +0300

    adding function drawing spring

--- main.py
+++ main.py
@@ -16,11 +16,6 @@ square(t, 'orange', 75, 50, 50)
 square(t, 'red', 75, -50, -50)
 square(t, 'yellow', 75, 150, 150)
 
-for i in range(8):
-    t.fd(50)  # количество шагов
-    t.lt(45)  # градус поворота
-    t.fd(50)
 
-t.goto(100, -50)
 
 done()
--- shapes.py
+++ shapes.py
@@ -8,3 +8,27 @@ def square(turtle, color, size, x, y):
         turtle.fd(size)
         turtle.rt(90)
     turtle.up()
+
+
+def octagon(turtle, color, size, x, y):
+    turtle.up()  # не рисовать
+    turtle.color(color)
+    turtle.goto(x, y)  # перемещение по координатам
+
+    turtle.down()  # начать рисовать
+    for i in range(8):
+        turtle.fd(size)  # количество шагов
+        turtle.lt(45)  # градус поворота
+    turtle.up()
+
+
+def spring(turtle, color, size, angle, x, y):
+    turtle.up()  # не рисовать
+    turtle.color(color)
+    turtle.goto(x, y)  # перемещение по координатам
+
+    for i in range(500):
+        turtle.fd(size)
+        turtle.lt(angle)
+        size += 10
+    turtle.up()
